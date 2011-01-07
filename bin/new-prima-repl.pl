038f7ba2b37ca3b1b2b29fa1f6dcd414cc9eb7b9
David Mertens <mertens@david-laptop>
2011-01-06 21:49:20 -0600

    Hackings...

---
+++ bin/new-prima-repl.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# The new-and-improved Prima-REPL, which uses
+# the class-based system for all of it's work!
+
+use Prima::REPL;
+my $app = Prima::REPL->new;
+$app->run;
+
--- lib/Prima/REPL.pm
+++ lib/Prima/REPL.pm
@@ -1,3 +1,12 @@
 # The basic class for the prima-repl application.
 
+=head1 NAME
+
+=cut
+
+package Prima::REPL;
+
+
+sub new {
+}
 
