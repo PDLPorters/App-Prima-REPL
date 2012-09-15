a68b3faa3eaada194cd698447f0d5c369adc9807
Joel Berger <joel.a.berger@gmail.com>
2012-09-15 10:26:40 -0500

    fix problem handling STDERR using this mechanism
    The problem was that the REPL::outwindow function was looking for print_to_terminal
    in the IO::OutWindow class, but it should be provided by the REPL (ie. handler) class
    for proper decoupling.

--- lib/App/Prima/REPL/IO/OutWindow.pm
+++ lib/App/Prima/REPL/IO/OutWindow.pm
@@ -1,35 +1,29 @@
-# Redirect standard output using this filehandle tie. Thanks to 
-# http://stackoverflow.com/questions/387702/how-can-i-hook-into-perls-print
-# for this one.
+use strict;
+use warnings;
+
+# classes for tying a handle to output via a handler package (default is REPL) 
+# and specifically to its 'outwindow' function
+
 package App::Prima::REPL::IO::OutWindow;
 use base 'Tie::Handle';
-use Symbol qw<geniosym>;
 use Carp;
 
 sub TIEHANDLE {
 	my $class = shift;
+	my $handler = shift || 'REPL';
+	my $outwindow = $handler->can('outwindow')
+		or croak "Output handler doesn't provide outwindow function!";
+
 	my $self = {
-		handler   => shift || 'REPL',
+		outwindow => $outwindow,
 		to_stderr => 0,
 	};
+
 	return bless $self, $class;
 }
 
-sub handler {
-	my $self = shift;
-	return $self->{handler};
-}
-
-sub outwindow {
-	my $self = shift;
-	my $outwindow = $self->handler->can('outwindow')
-		or croak "Output handler doesn't provide outwindow function!";
-	return $outwindow;
-}
-
-sub to_stderr {
-	return shift->{to_stderr};
-}
+sub outwindow { shift->{outwindow} }
+sub to_stderr { shift->{to_stderr} }
 
 # Printing to this tied file handle sends the output to the outwindow function.
 sub PRINT {
@@ -40,18 +34,13 @@ sub PRINT {
 sub PRINTF {
 	my $self = shift;
 	my $to_print = sprintf(@_);
-	my $outwindow = $self->outwindow->($self->to_stderr(), @_);
-}
-# This function provides access to the original stdout file handle
-sub print_to_terminal {
-	print main::STDOUT @_;
+	my $outwindow = $self->outwindow->($self->to_stderr, @_);
 }
 
 package App::Prima::REPL::IO::OutWindow::Err;
 our @ISA = qw(App::Prima::REPL::IO::OutWindow);
-# Override the to_stderr function; everything else should fall through via the
-# base class
 
+# same constructor as base class except set the to_stderr attribute
 sub TIEHANDLE {
 	my $class = shift;
 	my $self = $class->SUPER::TIEHANDLE(@_);
